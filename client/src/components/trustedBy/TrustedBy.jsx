33e09a429bd1a63fd83a4e39d3fe3f41e8839628
Jitin <jitin4155578@gmail.com>
2023-05-21 16:46:42 +0530

    MERN project

---
+++ client/src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./TrustedBy.scss";
+
+const TrustedBy = () => {
+  return (
+    <div className="trustedBy">
+      <div className="container">
+        <strong><span>Trusted by:</span></strong>
+        <img src="./img/lpu.png" alt="" />
+        <img src="./img/t1.png" alt="" />
+        <img src="./img/t2.png" alt="" />
+        <img src="./img/t3.png" alt="" />
+      
+        <img src="./img/t4.jpg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default TrustedBy;
---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import upload from "../../utils/upload";
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import "./Register.scss";
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
+
+function Register() {
+  const form = useRef();
+  const [file, setFile] = useState(null);
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    img: "",
+    country: "",
+    isSeller: false,
+    desc: "",
+  });
+  console.log(user);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUser((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleSeller = (e) => {
+    setUser((prev) => {
+      return { ...prev, isSeller: e.target.checked };
+    });
+  };
+  const sendEmail = async (e) => {
+    emailjs.sendForm('service_5142pah', 'template_9u9y4ql', form.current,'TyjjGTBzjkfgl9es8')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //const url = await upload(file);
+    
+    try {
+      await newRequest.post("/auth/register", {
+        ...user,
+        img: "./img/Formal.jpg",
+      });
+      sendEmail();
+      navigate("/")
+    } catch (err) {
+      console.log(err);
+    }
+    
+  };
+  
+
+  return (
+    <div className="register">
+      <form ref={form} onSubmit={handleSubmit}>
+        <div className="left">
+          <h1>Create a new account</h1>
+          <label htmlFor="">Username</label>
+          <input
+            name="username"
+            type="text"
+            placeholder="johndoe"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Password</label>
+          <input name="password" type="password" onChange={handleChange} />
+          <label htmlFor="">Profile Picture</label>
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+          <label htmlFor="">Country</label>
+          <input
+            name="country"
+            type="text"
+            placeholder="India"
+            onChange={handleChange}
+          />
+          <button type="submit">Register</button>
+        </div>
+        <div className="right">
+          <h1>I want to become a seller</h1>
+          <div className="toggle">
+            <label htmlFor="">Activate the seller account</label>
+            <label className="switch">
+              <input type="checkbox" onChange={handleSeller} />
+              <span className="slider round"></span>
+            </label>
+          </div>
+          <label htmlFor="">Phone Number</label>
+          <input
+            name="phone"
+            type="text"
+            placeholder="+91 7384948518"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Description</label>
+          <textarea
+            placeholder="A short description of yourself"
+            name="desc"
+            id=""
+            cols="30"
+            rows="10"
+            onChange={handleChange}
+          ></textarea>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
